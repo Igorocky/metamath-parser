c1581ac2fb54ff3d8561ac629f92fa5944f606e7
igorocky <123@gmail.com>
2021-10-09 13:23:15 +0200

    Implement parser of LabeledSequence.

--- src/main/kotlin/org/igye/metamathparser/Parsers.kt
+++ src/main/kotlin/org/igye/metamathparser/Parsers.kt
@@ -3,27 +3,112 @@ package org.igye.metamathparser
 import java.util.*
 import kotlin.collections.ArrayList
 
-object Parsers {
-    private const val SPACE = ' '
+data class ParserInput(val text:String, val begin:Int) {
+    fun charAtRel(i:Int): Char = text[toAbsolute(i)]
+    fun charAt(i:Int): Char = text[i]
+    fun proceed(n:Int) = this.copy(begin = begin+n)
+    fun currPositionStr():String = begin.toString()
+    fun toAbsolute(i:Int) = begin+i
+}
 
-    fun parseListOfConstants(inp:ParserInput): ParserOutput<ListOfConstants> {
-        if (inp.charAt(0) != '$' || inp.charAt(1) != 'c') {
+data class ParserOutput<T>(val result:T, val end:Int)
+
+data class NonLabeledSequence(val beginIdx:Int, val seqType:Char, val symbols:List<String>)
+data class LabeledSequence(val beginIdx:Int, val label:String, val seqType:Char, val symbols:List<String>)
+data class ConstantStmt(val beginIdx:Int, val symbols:List<String>)
+data class VariableStmt(val beginIdx:Int, val symbols:List<String>)
+data class DisjointStmt(val beginIdx:Int, val symbols:List<String>)
+data class FloatingStmt(val beginIdx:Int, val label:String, val typecode:String, val variable:String)
+
+object Parsers {
+    fun parseConstantStmt(inp:ParserInput): ParserOutput<ConstantStmt> {
+        if (inp.charAtRel(0) != '$' || inp.charAtRel(1) != 'c') {
             throw MetamathParserException("Cannot parse list of constants at ${inp.currPositionStr()}")
         }
         val listOfSymbols = parseListOfSymbols(inp.proceed(2))
         return ParserOutput(
-            result = ListOfConstants(symbols = listOfSymbols.result, beginIdx = inp.begin),
+            result = ConstantStmt(symbols = listOfSymbols.result, beginIdx = inp.begin),
             end = listOfSymbols.end
         )
     }
 
-    fun parseListOfSymbols(inp:ParserInput): ParserOutput<List<String>> {
+    fun parseVariableStmt(inp:ParserInput): ParserOutput<VariableStmt> {
+        if (inp.charAtRel(0) != '$' || inp.charAtRel(1) != 'v') {
+            throw MetamathParserException("Cannot parse list of variables at ${inp.currPositionStr()}")
+        }
+        val listOfSymbols = parseListOfSymbols(inp.proceed(2))
+        return ParserOutput(
+            result = VariableStmt(symbols = listOfSymbols.result, beginIdx = inp.begin),
+            end = listOfSymbols.end
+        )
+    }
+
+    fun parseDisjointStmt(inp:ParserInput): ParserOutput<DisjointStmt> {
+        if (inp.charAtRel(0) != '$' || inp.charAtRel(1) != 'd') {
+            throw MetamathParserException("Cannot parse DisjointStmt at ${inp.currPositionStr()}")
+        }
+        val listOfSymbols = parseListOfSymbols(inp.proceed(2))
+        return ParserOutput(
+            result = DisjointStmt(symbols = listOfSymbols.result, beginIdx = inp.begin),
+            end = listOfSymbols.end
+        )
+    }
+
+    fun parseLabeledSequence(inp:ParserInput): ParserOutput<LabeledSequence> {
+        val labelParserOutput = parsePrintable(inp)
+        if (labelParserOutput.result.isEmpty()) {
+            throw MetamathParserException("A label was expected at ${inp.currPositionStr()}")
+        }
+        val spaceParserOutput = parseWhitespace(inp.proceed(labelParserOutput.result.length))
+        if (spaceParserOutput.result.isEmpty()) {
+            throw MetamathParserException("A whitespace was expected between the label and the beginning of a list at ${inp.currPositionStr()}")
+        }
+        if (inp.charAt(spaceParserOutput.end+1) != '$') {
+            throw MetamathParserException("Cannot parse labeled sequence at ${inp.currPositionStr()}")
+        }
+        val seqType = inp.charAt(spaceParserOutput.end+2)
+        if (!inp.charAt(spaceParserOutput.end+3).isWhitespace()) {
+            throw MetamathParserException("A whitespace was expected between the beginning of a list at its first element at ${inp.currPositionStr()}")
+        }
+        val listOfSymbols = parseListOfSymbols(inp.proceed(spaceParserOutput.end+3-inp.begin))
+        return ParserOutput(
+            result = LabeledSequence(
+                beginIdx = inp.begin,
+                label = labelParserOutput.result,
+                seqType = seqType,
+                symbols = listOfSymbols.result
+            ),
+            end = listOfSymbols.end
+        )
+    }
+
+    private fun parsePrintable(inp:ParserInput): ParserOutput<String> {
+        return parseWhile(inp) {!it.isWhitespace()}
+    }
+
+    private fun parseWhitespace(inp:ParserInput): ParserOutput<String> {
+        return parseWhile(inp) {it.isWhitespace()}
+    }
+
+    private fun parseWhile(inp:ParserInput, predicate: (Char) -> Boolean): ParserOutput<String> {
+        val result = StringBuffer()
+        var i = 0
+        var currChar = inp.charAtRel(i)
+        while (predicate(currChar)) {
+            result.append(currChar)
+            i += 1
+            currChar = inp.charAtRel(i)
+        }
+        return ParserOutput(result = result.toString(), end = inp.toAbsolute(i-1))
+    }
+
+    private fun parseListOfSymbols(inp:ParserInput): ParserOutput<List<String>> {
         val symbols = ArrayList<String>()
         val currSymbol = StringBuffer()
         var i = 0
-        var currChar = inp.charAt(i)
+        var currChar = inp.charAtRel(i)
         while (currChar != '$') {
-            if (currChar == SPACE) {
+            if (currChar.isWhitespace()) {
                 if (currSymbol.isNotEmpty()) {
                     symbols.add(currSymbol.toString())
                     currSymbol.setLength(0)
@@ -32,10 +117,13 @@ object Parsers {
                 currSymbol.append(currChar)
             }
             i += 1
-            currChar = inp.charAt(i)
+            currChar = inp.charAtRel(i)
         }
-        if (inp.charAt(i+1) != '.') {
-            throw MetamathParserException("List of symbols must end with '$.' at ${inp.currPositionStr()}")
+        if (inp.charAtRel(i+1) != '.') {
+            throw MetamathParserException("A list of symbols must end with '$.' at ${inp.currPositionStr()}")
+        }
+        if (symbols.isEmpty()) {
+            throw MetamathParserException("A list of symbols is expected to have at least one element at ${inp.currPositionStr()}")
         }
         return ParserOutput(result = Collections.unmodifiableList(symbols), end = inp.toAbsolute(i+1))
     }
--- src/test/kotlin/org/igye/metamathparser/ParsersTest.kt
+++ src/test/kotlin/org/igye/metamathparser/ParsersTest.kt
@@ -6,10 +6,10 @@ import org.junit.Test
 
 internal class ParsersTest {
     @Test
-    fun parseListOfConstants_parses_list_of_constants() {
+    fun parseConstantStmt_parses_list_of_constants() {
         //when
-        val parserOutput: ParserOutput<ListOfConstants> =
-            Parsers.parseListOfConstants(ParserInput(text = "\$c 0 + = -> ( ) term wff |- \$.", begin = 0))
+        val parserOutput: ParserOutput<ConstantStmt> =
+            Parsers.parseConstantStmt(ParserInput(text = "\$c 0 + = -> ( ) term wff |- \$.", begin = 0))
 
         //then
         assertEquals(
@@ -17,5 +17,33 @@ internal class ParsersTest {
             parserOutput.result.symbols
         )
         assertEquals(29, parserOutput.end)
+    }
+
+    @Test
+    fun parseVariableStmt_parses_list_of_variables() {
+        //when
+        val parserOutput: ParserOutput<VariableStmt> =
+            Parsers.parseVariableStmt(ParserInput(text = "\$v t r s P Q \$.", begin = 0))
+
+        //then
+        assertEquals(
+            listOf("t", "r", "s", "P", "Q"),
+            parserOutput.result.symbols
+        )
+        assertEquals(14, parserOutput.end)
+    }
+
+    @Test
+    fun parseLabeledSequence_parses_labeled_list_of_symbols() {
+        //when
+        val parserOutput: ParserOutput<LabeledSequence> =
+            Parsers.parseLabeledSequence(ParserInput(text = "maj \$e |- ( P -> Q ) \$.", begin = 0))
+
+        //then
+        val labeledSequence = parserOutput.result
+        assertEquals("maj", labeledSequence.label)
+        assertEquals('e', labeledSequence.seqType)
+        assertEquals(listOf("|-", "(", "P", "->", "Q", ")"), labeledSequence.symbols)
+        assertEquals(22, parserOutput.end)
     }
 }
